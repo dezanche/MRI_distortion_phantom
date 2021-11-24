ec78b932ad40a8fff85c7c6b8fd79798a368d0ed
Nicola De Zanche <dezanche@ualberta.ca>
2021-11-23 17:31:09 -0700

    license update

--- Support_files/generate_bead_coordinates.m
+++ Support_files/generate_bead_coordinates.m
@@ -1,6 +1,18 @@
 % generates fiducials file (.fcsv) to import bead coordinates into 3D slicer
 % see https://www.slicer.org/wiki/Modules:Fiducials-Documentation-3.6
-% © 2021 Nicola De Zanche
+% Copyright © 2021 Nicola De Zanche
+
+%This program is free software: you can redistribute it and/or modify
+%it under the terms of the GNU General Public License as published by
+%the Free Software Foundation, either version 3 of the License, or any later version.
+
+%This program is distributed in the hope that it will be useful,
+%but WITHOUT ANY WARRANTY; without even the implied warranty of
+%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%GNU General Public License for more details.
+
+%You should have received a copy of the GNU General Public License
+%along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 % coordinate system is rotated to be compatible with 3D Slicer:
 % X --> R
